f101d78670244da13f1e5dfa6ad830c0fef205d1
AntoineDrsl <antoine.durussel@gmail.com>
2023-07-19 14:07:02 +0200

    :construction: UPDATE login

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,12 @@ import './Font.css';
 import './App.css';
 
 const socket = socketIO.connect('http://localhost:4000');
+
 function App() {
+  const userName = 'Antoine';
+  localStorage.setItem('userName', userName);
+  socket.emit('newUser', { userName, socketID: socket.id });
+
   return (
     <BrowserRouter>
       <div>
