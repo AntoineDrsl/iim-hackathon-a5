2b830842fc69053af124970c13b48d766c10ac9a
Allan <allan.duche@gmail.com>
2023-07-19 13:39:16 +0200

    create card and map

---
+++ client/src/components/UserList.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import data from '../assets/data.json';
+import Card from "./Card";
+
+const UserList = ({ socket }) => {
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+  };
+  
+  return (
+    <>
+      {
+        data.map((user, index) => (
+          <Card
+            key={index}
+            socket={socket}
+            img={''}
+            name={user.nom}
+            school={user.ecole}
+            promo={user.promotion}
+            formation={'IIM'}
+            entreprise={user.entreprise}
+            poste={user.poste}
+          />
+        ))
+      }
+    </>
+  );
+};
+
+export default UserList;
