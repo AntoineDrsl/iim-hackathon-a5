4946d0fae129f4d0d4f2b27d8f0f1e037965f156
AntoineDrsl <antoine.durussel@gmail.com>
2023-07-19 10:46:27 +0200

    :sparkles: FEAT chat

---
+++ client/src/components/ChatPage.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import ChatBar from './ChatBar';
+import ChatBody from './ChatBody';
+import ChatFooter from './ChatFooter';
+
+const ChatPage = ({ socket }) => {
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		socket.on('messageResponse', (data) => {
+			console.log('je suis là')
+			setMessages([...messages, data])
+		});
+	}, [socket, messages]);
+
+  return (
+    <div className="chat">
+			<ChatBar socket={socket} />
+      <div className="chat__main">
+        <ChatBody messages={messages} />
+        <ChatFooter socket={socket} />
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
--- server/index.js
+++ server/index.js
@@ -1,33 +1,46 @@
+const PORT = 4000;
 const express = require('express');
 const app = express();
-const PORT = 4000;
-
-//New imports
-const http = require('http').Server(app);
-const cors = require('cors');
-
-app.use(cors());
-
-const socketIO = require('socket.io')(http, {
+const server = require('http').createServer(app);
+const socketIO = require('socket.io')(server, {
     cors: {
         origin: "http://localhost:3000"
     }
 });
 
-//Add this before the app.get() block
+// Cors
+const cors = require('cors');
+app.use(cors());
+
+// Socket
+let users = [];
 socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
+
+	//Listens and logs the message to the console
+	socket.on('message', (data) => {
+		socketIO.emit('messageResponse', data);
+	});
+
+	socket.on('newUser', (data) => {
+		users.push(data);
+		socketIO.emit('newUserResponse', users);
+	});
+
     socket.on('disconnect', () => {
       console.log('🔥: A user disconnected');
+	  users = users.filter((user) => user.socketID !== socket.id);
+	  socketIO.emit('newUserResponse', users);
+	  socket.disconnect();
     });
 });
 
+// Test route
 app.get('/api', (req, res) => {
   res.json({
     message: 'Hello world',
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+// Launch server
+server.listen(PORT, () => console.log('Server started at port : ' + PORT));
